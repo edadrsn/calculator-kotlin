5c4fcb71e218acad076c9d6cb8703531adb20930
Eda Dursun <edadrsn929@gmail.com>
2025-03-02 19:48:14 +0300

    addition and subtraction functions created

--- app/src/main/java/com/example/kotlincalculator/MainActivity.kt
+++ app/src/main/java/com/example/kotlincalculator/MainActivity.kt
@@ -12,8 +12,9 @@ import com.example.kotlincalculator.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
-
+    var number1: Double? = null
+    var number2: Double? = null
+    var result: Double? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,27 +24,41 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
+    }
+
+    fun add(view: View) {
+        number1 = binding.numOne.text.toString().toDoubleOrNull()
+        number2 = binding.numTwo.text.toString().toDoubleOrNull()
+        if(number1!=null && number2!=null){
+            result=number1!!+number2!!
+            binding.txtResult.text="RESUL: $result"
+        }
+        else{
+            Toast.makeText(this,"Don't leave empty numbers",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun sub(view: View) {
+        number1=binding.numOne.text.toString().toDoubleOrNull()
+        number2=binding.numTwo.text.toString().toDoubleOrNull()
+        if(number1!=null && number2!=null){
+            result=number1!!-number2!!
+            binding.txtResult.text="RESULT: $result"
+        }
+
 
     }
 
-    fun add(view:View){
+    fun mult(view: View) {
 
     }
 
-    fun sub(view:View){
-
-
-    }
-    fun mult(view:View){
-
-    }
-
-    fun div(view:View){
+    fun div(view: View) {
 
 
     }
 
-    fun reset(view:View){
+    fun reset(view: View) {
         binding.numOne.setText("")
         binding.numTwo.setText("")
         binding.txtResult.setText("")
